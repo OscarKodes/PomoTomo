046e9316804bb85faa437588136bc4e2bdda7a26
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-20 17:11:48 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+      app     = express(),
+      ejs     = require("ejs"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose");
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server is listening on port 3000");
+});
