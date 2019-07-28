74f95e460dec3e31fd7f873806234ad6b333f185
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-28 09:52:21 -0400

    Fix google and env file issues

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const User = require("../models/user");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/pomo",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
