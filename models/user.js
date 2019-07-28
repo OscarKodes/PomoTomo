c34389b5e9c2b747bd8cd67aae57449fa8643976
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-28 10:19:06 -0400

    Embed daySchema into userSchema and add data types to record sound options.

--- models/user.js
+++ models/user.js
@@ -3,10 +3,30 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require("mongoose-findorcreate");
 
+const daySchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  pomos: {
+    type: Number,
+    default: 0
+  }
+});
+
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  googleId: String
+  googleId: String,
+  days: [daySchema],
+  alarmSound: {
+    type: String,
+    default: "Default"
+  },
+  pomoUpSound: {
+    type: String,
+    default: "Default"
+  }
 });
 
 userSchema.plugin(passportLocalMongoose);
