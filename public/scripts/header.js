deaa61e6f3af3ff0d947a4939dd770dbe49e8985
Oscar Ko <OscarKoEmail@gmail.com>
2019-07-31 17:24:47 -0400

    Stylize register pop-up form and add cancel button

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,12 @@
 let registerBtn = document.querySelector("#register-btn");
 let registerForm = document.querySelector("#register-form");
+let closeBtn = document.querySelector(".fa-window-close");
+let outsideForm = document.querySelector(":not(#register-card)");
 
 registerBtn.addEventListener("click", function(){
   registerForm.classList.remove("invisible");
+});
+
+closeBtn.addEventListener("click", function(){
+  registerForm.classList.add("invisible");
 });
