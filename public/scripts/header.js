ddc6e7f764dded89b666f5590559386294e96189
Oscar Ko <OscarKoEmail@gmail.com>
2019-08-01 13:29:51 -0400

    Add login popup button form functionality and stylize

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,12 +1,23 @@
 let registerBtn = document.querySelector("#register-btn");
 let registerForm = document.querySelector("#register-form");
-let closeBtn = document.querySelector(".fa-window-close");
-let outsideForm = document.querySelector(":not(#register-card)");
+let loginBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector("#login-form");
+let closeBtn = document.querySelectorAll(".fa-window-close");
 
 registerBtn.addEventListener("click", function(){
   registerForm.classList.remove("invisible");
 });
 
-closeBtn.addEventListener("click", function(){
+loginBtn.addEventListener("click", function(){
+  loginForm.classList.remove("invisible");
+});
+
+// the register form appears above the login form in the ejs file
+// so it'll come first the the querySelectorAll array
+closeBtn[0].addEventListener("click", function(){
   registerForm.classList.add("invisible");
+});
+
+closeBtn[1].addEventListener("click", function(){
+  loginForm.classList.add("invisible");
 });
